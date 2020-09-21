ca4c9a74d8eeaf4ec223692f1d82a6e4822a197b
Julio Gutierrez <a01282575@itesm.mx>
2020-09-20 21:12:49 -0500

    Poke api requests done

--- index.js
+++ index.js
@@ -1,54 +1,67 @@
-var total_cost = 0;
-let item_name = document.querySelector("#item-name");
-let item_value = document.querySelector("#item-value");
-let item_total = document.querySelector("#total-value");
-let item_button = document.querySelector("#add-item");
+var weight = 0
+let poke_name = document.querySelector("#poke-name")
+let weight_total = document.querySelector("#total-value")
+let search_button = document.querySelector("#poke-search")
 let total = document.querySelector("#total-value")
 
-total.textContent = 0;
-item_button.addEventListener("click", (_) => {
-  if(item_name.value != "" && item_value.value != ""){
-    item_name.classList.remove("warning")
-    item_value.classList.remove("warning")
-    add_item_to_ul(item_name.value, item_value.value, total);
-  }else{
-    item_name.classList.add("warning")
-    item_value.classList.add("warning")
-  }
+total.textContent = 0
+
+search_button.addEventListener("click", (_) => {
+//   if(poke_name.value != ""){
+//     // item_name.classList.remove("warning")
+//     // item_value.classList.remove("warning")
+       add_item_to_ul(poke_name.value, total);
+//   }else{
+//     item_name.classList.add("warning")
+//     item_value.classList.add("warning")
+//   }
 })
 
 function get_element_li (name, price, total_value) {
-  let li = document.createElement('li');
-  let it_name = document.createElement('span');
-  let value = document.createElement('span');
+  let li = document.createElement('li')
+  let poke_name = document.createElement('span')
+  let poke_weight = document.createElement('span')
   let button = document.createElement('button')
 
-  button.classList.add("remove")
-  button.innerText = "remove";
-  it_name.textContent = name;
-  value.textContent = price;
+  //button.classList.add("remove")
+  button.innerText = "remove"
+  poke_name.textContent = name
+  poke_weight.textContent = price
 
   button.addEventListener("click", (_) => {
     remove_item(li, total_value)
   });
 
-  li.appendChild(it_name)
-  li.appendChild(value)
+  li.appendChild(poke_name)
+  li.appendChild(poke_weight)
   li.appendChild(button);
   return li
 }
 
-function add_item_to_ul(item_name, item_price, total_value){
-  total_cost += parseInt(item_price,10)
-  let li = get_element_li(item_name,item_price, total_value)
-  let ul = document.getElementById("list")
-  ul.appendChild(li)
-  total_value.textContent = total_cost
+async function get_pokemon(name){
+    try{
+        const res = await axios.get('https://pokeapi.co/api/v2/pokemon/'+ name)
+        return res.data
+    } catch(e){
+        console.log("error")
+    }
+};
+
+function add_item_to_ul(name, total){
+  let poke_data;
+  (async () => {
+    poke_data  = await get_pokemon(name)
+    weight += parseInt(poke_data.weight,10)
+    let li = get_element_li(poke_data.name,poke_data.weight, total)
+    let ul = document.getElementById("list")
+    ul.appendChild(li)
+    total.textContent = weight
+  })()
 }
 
 let remove_item  = (node_to_remove, total_value) => {
   let ul = document.getElementById("list")
-  total_cost -= parseInt(node_to_remove.children[1].innerText)
-  total_value.textContent = total_cost
+  weight -= parseInt(node_to_remove.children[1].innerText)
+  total_value.textContent = weight
   ul.removeChild(node_to_remove)
 }
